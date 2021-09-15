b962cec6b1c0bc08559cfdb97f3c7679d09988e8
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-09-15 16:02:36 +0200

    display likes and add likes icon

--- src/index.js
+++ src/index.js
@@ -17,11 +17,14 @@ mealsHolder.className = "mealsHolder";
 const displayMeals = (list) => {
   list.forEach((meal) => {
     const mealCard = document.createElement("div");
+    const likeIcon = document.createElement("i");
     const mealImg = document.createElement("img");
     const mealTitle = document.createElement("h3");
     const mealRecipe = document.createElement("p");
+    let mealLikes = document.createElement("span");
+    const h3Wrapper = document.createElement("div");
     const recipe =
-    meal.strInstructions.slice(0, 50) +
+    meal.strInstructions.slice(0, 10) +
     (meal.strInstructions.length > 50 ? "..." : "");
     const mealVideoLink = document.createElement("a");
     
@@ -38,8 +41,12 @@ const displayMeals = (list) => {
 
     mealVideoLink.setAttribute('href', meal.strYoutube);
     mealVideoLink.textContent = `Youtube Video`;
-
-    mealCard.append(mealImg, mealTitle, mealRecipe, mealVideoLink);
+    mealLikes.innerText = '0 Likes';
+    mealLikes.className = "meal-likes";
+    likeIcon.className = "fas fa-heart";
+    h3Wrapper.className = "likes-display";
+    h3Wrapper.append(mealTitle, likeIcon);
+    mealCard.append(mealImg,h3Wrapper, mealLikes, mealRecipe, mealVideoLink);
     mealsHolder.appendChild(mealCard);
   });
 };
@@ -51,3 +58,7 @@ const fetchMeals = async () => {
 };
 
 fetchMeals();
+
+fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DG1557loKgI2XXAUI0g2/likes/')
+.then(res => res.json())
+.then(data => console.log(data))
