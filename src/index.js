e60197a26fe251a4d4d8d959e1434f884dc989f3
Atsighi Bright <brytebee@gmail.com>
2021-09-15 13:09:02 +0100

    Add comment popup

--- src/index.js
+++ src/index.js
@@ -21,10 +21,11 @@ const displayMeals = (list) => {
     const mealTitle = document.createElement("h3");
     const mealRecipe = document.createElement("p");
     const recipe =
-    meal.strInstructions.slice(0, 50) +
-    (meal.strInstructions.length > 50 ? "..." : "");
+      meal.strInstructions.slice(0, 50) +
+      (meal.strInstructions.length > 50 ? "..." : "");
     const mealVideoLink = document.createElement("a");
-    
+    const commentButton = document.createElement("button");
+
     mealCard.className = "mealCard";
 
     mealImg.setAttribute("src", meal.strMealThumb);
@@ -36,11 +37,15 @@ const displayMeals = (list) => {
 
     mealRecipe.textContent = recipe;
 
-    mealVideoLink.setAttribute('href', meal.strYoutube);
+    mealVideoLink.setAttribute("href", meal.strYoutube);
     mealVideoLink.textContent = `Youtube Video`; //create a popup to play in-app rather that redirect (v2.0)
 
-    mealCard.append(mealImg, mealTitle, mealRecipe, mealVideoLink);
-    commentPopUp(mealCard);
+    commentButton.textContent = "Comments";
+    commentButton.addEventListener("click", () => commentPopUp(meal));
+
+    // mealCard.append(mealImg, mealTitle, mealRecipe, mealVideoLink, commentButton);
+    mealCard.append(mealImg, mealTitle, mealRecipe, commentButton);
+    // commentPopUp(mealCard);
     mealsHolder.appendChild(mealCard);
   });
 };
@@ -53,10 +58,47 @@ const fetchMeals = async () => {
 
 fetchMeals();
 
-const commentPopUp = (card) => {
-  card.addEventListener('click', () => {
-    const popup = open('', 'Popup', 'width=800,height=700');
-    popup.className = 'popup'; //class stying not working.
-    popup.document.body.appendChild(card);
-  });
-}
+// // Add the popup to the enntire card 
+// const commentPopUp = (card) => {
+//   card.addEventListener('click', () => {
+//     const popup = open('', 'Popup', 'width=800,height=700');
+//     popup.className = 'popup'; //class stying not working.
+//     popup.document.body.appendChild(card);
+//   });
+// }
+
+
+const commentPopUp = (meal) => {
+  const mealCard = document.createElement("div");
+  const mealImg = document.createElement("img");
+  const mealTitle = document.createElement("h3");
+  const mealRecipe = document.createElement("p");
+  const mealVideoLink = document.createElement("a");
+  const commentButton = document.createElement("button");
+  
+  mealCard.className = "mealCard";
+
+  mealImg.setAttribute("src", meal.strMealThumb);
+  mealImg.setAttribute("alt", meal.strMeal);
+  mealImg.className = "meal-img";
+
+  mealTitle.textContent = meal.strMeal;
+  mealTitle.className = "meal-title";
+
+  mealRecipe.textContent = meal.strInstructions;
+
+  mealVideoLink.setAttribute("href", meal.strYoutube);
+  mealVideoLink.textContent = `Youtube Video`; //create a popup to play in-app rather that redirect (v2.0)
+
+  commentButton.textContent = "Comments";
+  commentButton.className = 'show btn';
+  mealCard.append(
+    mealImg,
+    mealTitle,
+    mealRecipe,
+    mealVideoLink,
+    commentButton
+  );
+  const popup = open("", "Popup", "width=800,height=700");
+  popup.document.body.appendChild(mealCard);
+};
